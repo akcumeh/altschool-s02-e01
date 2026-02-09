be14259299c665622a39b7915f1518cee19575b1
Angel Umeh <akcumeh@gmail.com>
2026-02-09 23:31:22 +0100

    Create routes & middleware

---
+++ routes/blogRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const blogController = require('../controllers/blogController');
+const auth = require('../middleware/auth');
+
+router.get('/blogs', blogController.getPublishedBlogs);
+router.get('/blogs/id', blogController.getBlogById);
+
+router.post('/blogs', auth, blogController.createPost);
+router.get('/user/blogs', auth, blogController.getUserBlogs);
+router.patch('/blogs/:id/state', auth, blogController.updateBlogState);
+router.put('/blogs/:id', auth, blogController.updateBlog);
+router.delete('/blogs/:id', auth, blogController.deleteBlog);
+
+module.exports = router;
