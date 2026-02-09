d7e3ae9ed2cdc10a8ce878408b6083f254e554cf
Angel Umeh <akcumeh@gmail.com>
2026-02-09 16:08:28 +0100

    Project setup

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
